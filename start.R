5898d2655e9d4a3625f8df7e0478544fb8ef415e
favstats <fabio.votta@gmail.com>
2024-09-26 16:31:12 +0200

    update

--- start.R
+++ start.R
@@ -224,12 +224,12 @@ dir("_site", full.names = T) %>% keep(~str_detect(.x, "qmd")) %>% keep(~str_dete
 #   })
 # 
 # if (Sys.info()[["effective_user"]] == "fabio" | Sys.info()[["effective_user"]] == "favoo") {
-#   # system("git pull")
-#   gert::git_pull()
-#   # system("git add -A")
-#   # system('git commit -m "update"')
-#   # system("git push")
-#   gert::git_add(".")
-#   gert::git_commit("update")
-#   gert::git_push()
+  # system("git pull")
+  # gert::git_pull()
+  # # system("git add -A")
+  # # system('git commit -m "update"')
+  # # system("git push")
+  # gert::git_add(".")
+  # gert::git_commit("update")
+  # gert::git_push()
 # }
